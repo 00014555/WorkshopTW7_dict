d52b99eaec939d98be4a63287d4bdad2d19204ba
00014555 <wiut00014555@gmail.com>
2021-11-06 12:22:10 +0300

    1stcommit

---
+++ main.py
@@ -0,0 +1,45 @@
+import statistics
+orders = [1.99, 2.49, 1.19, 1.99]
+average = statistics.mean(orders)
+print("'Pomodorini/Tomatten/Tomatoes 250g pack': " + str(round(average, 2)) + ",")
+
+orders = [0.69, 0.79, 0.89, 0.69]
+average = statistics.mean(orders)
+print("'Capperi/Kappertjes/Capers': " + str(round(average, 2)) + ",")
+
+orders = [6.59, 7.29, 5.99, 6.39]
+average = statistics.mean(orders)
+print("'Olive oil extra virgin 750ml': " + str(round(average, 2)) + ",")
+
+orders = [0.99, 0.89, 0.99, 0.99]
+average = statistics.mean(orders)
+print("'Prezzemolo/Peterselie/Parsley': " + str(round(average, 2)) + ",")
+
+orders = [0.99, 0.89, 0.89, 0.89]
+average = statistics.mean(orders)
+print("'Coriandolo/Koriander/Coriander': " + str(round(average, 2)) + ",")
+
+orders = [2.29, 2.99, 2.99, 2.09]
+average = statistics.mean(orders)
+print("'Pasta Rummo 500g (Conchiglioni Rigati)': " + str(round(average, 2)) + ",")
+
+orders = [4.99, 4.99, 4.99, 4.99]
+average = statistics.mean(orders)
+print("'Salmone/Zalm/Salmon 250g': " + str(round(average, 2)) + ",")
+
+orders = [0.99, 0.89, 0.99, 1.09]
+average = statistics.mean(orders)
+print("'Aglio/Knoflook/Garlic': " + str(round(average, 2)) + ",")
+
+orders = [2.79, 3.49, 2.99, 3.09]
+average = statistics.mean(orders)
+print("'Olive/Olijven/Olives (Taggiasche)': " + str(round(average, 2)) + ",")
+
+orders = [14.95, 13.99, 14.18, 16.69]
+average = statistics.mean(orders)
+print("'Vino Bianco/Wit Wijn/White Wine (Greco di Tufo)': " + str(round(average, 2)) + ",")
+
+
+orders = [2.79, 3.09, 3.39, 2.79]
+average = statistics.mean(orders)
+print("'Caffe Lavazza (Black) 250g': " + str(round(average, 2)) + ",")
